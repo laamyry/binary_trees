d41161f8c3ecb52083a21fff5c6aa64bf31c070f
laamyry <slamiri10@gmail.com>
2023-08-30 18:07:41 +0100

    laamyry py

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *n_node;
 
 	n_node = malloc(sizeof(binary_tree_t));
-	if (n_node == 0)
+	if (!n_node)
 	{
 		return (NULL);
 	}
