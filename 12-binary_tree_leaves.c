6dabb74146771ff8d3fe9544e22acc838311557a
laamyry <slamiri10@gmail.com>
2023-09-01 22:02:22 +0100

    laamyry py

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+/**
+* binary_tree_leaves - counts the leaves in a binary tree.
+*
+* @tree: pointer to the root node of the tree to count the number of leaves.
+* Return: If tree is NULL, the function must return 0.
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
+	}
+
+}
+
