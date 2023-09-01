008052c22113b83295650f64ca460c2c611c1dcd
laamyry <slamiri10@gmail.com>
2023-09-01 03:41:34 +0100

    laamyry py

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,24 @@
 #include "binary_trees.h"
+/**
+* binary_tree_depth - measures the dep of a node in a binary tree.
+*
+* @tree: pointer to the node to measure the dep.
+* Return: If tree is NULL, your function must return 0.
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t dep = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	while (tree->parent)
+	{
+		dep++;
+		tree = tree->parent;
+	}
+
+	return (dep);
+}
