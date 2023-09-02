f816770465e2c645c9bf2ebc0406e2fc1896c5ad
laamyry <slamiri10@gmail.com>
2023-09-02 01:44:16 +0100

    laamyry py

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,1 +1,62 @@
 #include "binary_trees.h"
+/**
+* binary_tree_nodes - counts the nodes with at least 1 child in a binary tree.
+*
+* @tree:pointer to the root node of the tree to count the number of nodes.
+* Return: If tree is NULL, the function must return 0.
+* A NULL pointer is not a node.
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+    {
+		return (0);
+    }
+    else
+    {
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+    }
+}
+
+/**
+* binary_tree_leaves - counts the leaves in a binary tree.
+*
+* @tree: pointer to the root node of the tree to count the number of leaves.
+* Return: If tree is NULL, the function must return 0.
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	else if (!(tree->left) && !(tree->right))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+
+}
+
+/**
+* binary_tree_size - measures the size of a binary tree.
+*
+* @tree: pointer to the root node of the tree to measure the size.
+* Return: If tree is NULL, the function must return 0.
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	}
+
+}
